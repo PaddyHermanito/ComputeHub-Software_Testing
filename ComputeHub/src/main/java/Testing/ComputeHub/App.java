abaccd155a92c0f5a68a2057fbbdf82a006ffd90
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-25 07:59:28 +0700

    login success

--- ComputeHub/src/main/java/Testing/ComputeHub/App.java
+++ ComputeHub/src/main/java/Testing/ComputeHub/App.java
@@ -1,6 +1,8 @@
 package Testing.ComputeHub;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 /**
@@ -14,9 +16,24 @@ public class App
     	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     	
     	WebDriver driver=new ChromeDriver();
-    	driver.get("http://www.google.com");
+    	driver.get("http://localhost:5173/");
+    	driver.manage().window().maximize();
     	
-    	Thread.sleep(1000);
+    	WebElement login = driver.findElement(By.id("login"));
+		login.click();
+		
+		WebElement email = driver.findElement(By.id("email"));
+		email.click();
+		email.sendKeys("ryanko.4903@gmail.com");
+		
+		WebElement pass= driver.findElement(By.id("pass"));
+		pass.sendKeys("123456");
+		
+		WebElement loginButton = driver.findElement(By.id("login"));
+		loginButton.click();
+    	
+//    	Thread.sleep(1000);
     	driver.quit();
+    	System.out.println("Done");
     }
 }
