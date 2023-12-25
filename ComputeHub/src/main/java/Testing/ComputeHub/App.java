af278515996fa967946b4c851290c82147d9b57e
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-25 12:48:05 +0700

    TC1 + TC2 + TC4
    pass salah, email salah, login success

--- ComputeHub/src/main/java/Testing/ComputeHub/App.java
+++ ComputeHub/src/main/java/Testing/ComputeHub/App.java
@@ -1,9 +1,14 @@
 package Testing.ComputeHub;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;	
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 /**
  * Hello world!
@@ -18,21 +23,53 @@ public class App
     	WebDriver driver=new ChromeDriver();
     	driver.get("http://localhost:5173/");
     	driver.manage().window().maximize();
+//    	WebDriverWait wait = new WebDriverWait(driver, 10); // Timeout set to 10 seconds
     	
     	WebElement login = driver.findElement(By.id("login"));
 		login.click();
 		
 		WebElement email = driver.findElement(By.id("email"));
-		email.click();
 		email.sendKeys("ryanko.4903@gmail.com");
 		
 		WebElement pass= driver.findElement(By.id("pass"));
-		pass.sendKeys("123456");
+		pass.sendKeys("123asdf");
 		
 		WebElement loginButton = driver.findElement(By.id("login"));
 		loginButton.click();
     	
-//    	Thread.sleep(1000);
+    	Thread.sleep(500);
+//    	wait.until(ExpectedConditions.alertIsPresent());
+    	// Switch ke alert
+        Alert alert = driver.switchTo().alert();
+
+        // Ambil teks dari alert
+        String alertText = alert.getText();
+//        System.out.println("Alert Text: " + alertText);
+     // Terima (klik OK) pada alert
+        alert.accept();
+        
+     // Switch back to the default content
+        driver.switchTo().defaultContent();
+        
+        email.clear();
+        email.sendKeys("paddy.h21@mhs.istts.ac.id");
+        loginButton.click();
+        Thread.sleep(500);
+        
+        alert = driver.switchTo().alert();
+
+        // Ambil teks dari alert
+        alertText = alert.getText();
+//        System.out.println("Alert Text: " + alertText);
+     // Terima (klik OK) pada alert
+        alert.accept();
+        
+        email.clear();
+        email.sendKeys("ryanko.4903@gmail.com");
+        pass.clear();
+        pass.sendKeys("123456");
+        loginButton.click();
+        Thread.sleep(500);
     	driver.quit();
     	System.out.println("Done");
     }
