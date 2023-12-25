5160348add12510bd478b98ebb4a35beca87c67e
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-25 21:00:32 +0700

    TC14 + TC15
    Cek Cari Barang berdasarkan keyword
    Cek Sort barang berdasarkan category

--- ComputeHub/src/main/java/Testing/ComputeHub/App.java
+++ ComputeHub/src/main/java/Testing/ComputeHub/App.java
@@ -4,10 +4,13 @@ import java.time.Duration;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;	
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -18,6 +21,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  */
 public class App 
 {
+    /**
+     * @param args
+     * @throws InterruptedException
+     */
     public static void main( String[] args ) throws InterruptedException
     {
     	System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
@@ -39,11 +46,12 @@ public class App
 		WebElement loginButton = driver.findElement(By.id("login"));
 		loginButton.click();
     	
-    	Thread.sleep(500);
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+//    	Thread.sleep(500);
 //    	wait.until(ExpectedConditions.alertIsPresent());
     	// Switch ke alert
-        Alert alert = driver.switchTo().alert();
+        alert = driver.switchTo().alert();
 
         // Ambil teks dari alert
         String alertText = alert.getText();
@@ -57,7 +65,10 @@ public class App
         email.clear();
         email.sendKeys("paddy.h21@mhs.istts.ac.id");
         loginButton.click();
-        Thread.sleep(500);
+        
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        alert = wait.until(ExpectedConditions.alertIsPresent());
+//        Thread.sleep(500);
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         
         alert = driver.switchTo().alert();
@@ -67,24 +78,45 @@ public class App
 //        System.out.println("Alert Text: " + alertText);
      // Terima (klik OK) pada alert
         alert.accept();
-        
-        email.clear();
-        email.sendKeys("admin@gmail.com");
-        pass.clear();
-        pass.sendKeys("admin1");
-        loginButton.click();
-        Thread.sleep(500);
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement logout=driver.findElement(By.id("logout"));
-        logout.click();
-        Thread.sleep(500);
+        //TC3, admin
+//        email.clear();
+//        email.sendKeys("admin@gmail.com");
+//        pass.clear();
+//        pass.sendKeys("admin1");
+//        loginButton.click();
+//        Thread.sleep(500);
+//        WebElement logout=driver.findElement(By.id("logout"));
+//        logout.click();
+//        Thread.sleep(500);
 //        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         email.clear();
         email.sendKeys("ryanko.4903@gmail.com");
         pass.clear();
         pass.sendKeys("123456");
         loginButton.click();
-        Thread.sleep(500);
+        
+        
+//        WebElement cari = driver.findElement(By.id("searchItem"));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement cari = wait.until(ExpectedConditions.elementToBeClickable(By.id("searchItem")));
+        
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+//        Point position = driver.manage().window().getPosition();
+//        driver.manage().window().setPosition(new Point(0,0));
+        WebElement kaki = driver.findElement(By.tagName("footer"));
+        WebElement kategori = driver.findElement(By.id("category"));
+        WebElement bestDeal = driver.findElement(By.id("best_deal"));
+        Actions actions = new Actions(driver);
+        actions.moveToElement(bestDeal).perform();
+//        Thread.sleep(3000);
+//        actions.moveToElement(kaki, 0, -600).perform();
+//        actions.moveToElement(kategori).perform();
+    	cari.click();
+    	cari.sendKeys("rtx");
+    	WebElement cariBtn = driver.findElement(By.id("searchButton"));
+    	cariBtn.click();
+        Thread.sleep(2000);
     	driver.quit();
     	System.out.println("Done");
     }
