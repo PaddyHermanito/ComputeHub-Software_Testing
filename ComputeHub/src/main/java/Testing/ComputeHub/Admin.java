3d6414acb4cfc519369ae74e37a03090405e1356
PaddyHermanito <paddy.hermanito.suhartanto93@gmail.com>
2023-12-29 14:40:59 +0700

    TC9
    Cek fitur Edit item

--- ComputeHub/src/main/java/Testing/ComputeHub/Admin.java
+++ ComputeHub/src/main/java/Testing/ComputeHub/Admin.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -66,7 +67,35 @@ public class Admin {
         WebElement customer = wait.until(ExpectedConditions.elementToBeClickable(By.id("customer")));
         customer.click();
         
-        
+        WebElement Master_Item = wait.until(ExpectedConditions.elementToBeClickable(By.id("Master Item")));
+        Master_Item.click();
+        Actions actions = new Actions(driver);
+        Thread.sleep(2000);
+        WebElement actions0=wait.until(ExpectedConditions.presenceOfElementLocated(By.id("actions0"))).findElement(By.id("edit0"));        
+//        WebElement actions0=driver.findElement(By.id("actions0"));
+//        WebElement edit0=actions0.findElement(By.id("edit0"));
+        actions.moveToElement(actions0).perform();
+//        WebElement edit0 = wait.until(ExpectedConditions.elementToBeClickable(By.id("edit0")));
+//        WebElement edit0 = actions0.findElement(By.id("edit0"));
+//        actions.moveToElement(edit0);
+//        edit0.click();
+        actions0.click();
+        WebElement stock = wait.until(ExpectedConditions.elementToBeClickable(By.id("stock")));
+        stock.sendKeys("1");
+        WebElement ok = wait.until(ExpectedConditions.elementToBeClickable(By.id("ok")));
+        ok.click();
+        alert = wait.until(ExpectedConditions.alertIsPresent());
+        alert = driver.switchTo().alert();
+
+        // Ambil teks dari alert
+        alertText = alert.getText();
+//        System.out.println("Alert Text: " + alertText);
+     // Terima (klik OK) pada alert
+        alert.accept();
+     // Switch back to the default content
+        driver.switchTo().defaultContent();
+        actions0=wait.until(ExpectedConditions.presenceOfElementLocated(By.id("actions0"))).findElement(By.id("edit0"));     
+        actions.moveToElement(actions0).perform();
         Thread.sleep(2000);
     	driver.quit();
     	System.out.println("Done");
